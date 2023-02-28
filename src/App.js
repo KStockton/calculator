9c0ff30de6be07932b582c501eb9cef7ecb7d278
kingstockton@gmail.com <>
2023-02-27 18:56:52 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state, action) {
+
 }
-
-export default App;
+export default function App() {
+  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'></div>
+        <div className='current-operand'></div>
+        </div>
+        <button className='span-two'>AC</button>
+        <button>Del</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+    <button className='span-two'> = </button>
+      </div>
+  )
+}
